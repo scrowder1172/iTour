ac6b001de38e4169a58506dbc7f3d49935eacc82
Scott Crowder <scrowder@gmail.com>
2023-11-21 13:40:08 -0600

    Initial Commit

---
+++ iTour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTour
+//
+//  Created by SCOTT CROWDER on 11/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
