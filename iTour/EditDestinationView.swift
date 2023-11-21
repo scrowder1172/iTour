12b6265656a36f3ecc4f8c3a924882426af97340
Scott Crowder <scrowder@gmail.com>
2023-11-21 14:16:11 -0600

    create workflow for editing a destination

--- iTour/EditDestinationView.swift
+++ iTour/EditDestinationView.swift
@@ -6,13 +6,47 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct EditDestinationView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @Bindable var destination: Destination
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Name", text: $destination.name)
+                TextField("Details", text: $destination.details, axis: .vertical)
+                DatePicker("Date", selection: $destination.date)
+                
+                Section("Priority") {
+                    Picker("Priority", selection: $destination.priority) {
+                        Text("Meh").tag(1)
+                        Text("Maybe").tag(2)
+                        Text("Must").tag(3)
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+            }
+            .navigationTitle("Edit Destination")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
 #Preview {
-    EditDestinationView()
+    do {
+        let config: ModelConfiguration = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container: ModelContainer = try ModelContainer(for: Destination.self, configurations: config)
+        let example: Destination = Destination(name: "Example Destination", details: "Example details go here and will automatically expand vertically as they are edited.")
+        
+        return EditDestinationView(destination: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
 }
