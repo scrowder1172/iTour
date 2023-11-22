a07f8456ec64a4c0a215fefe67b8afa5275b76d2
Scott Crowder <scrowder@gmail.com>
2023-11-21 20:01:01 -0600

    add file

---
+++ iTour/SightsView.swift
@@ -0,0 +1,18 @@
+//
+//  SightsView.swift
+//  iTour
+//
+//  Created by SCOTT CROWDER on 11/21/23.
+//
+
+import SwiftUI
+
+struct SightsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SightsView()
+}
